26e7e330965bf9bc4b35e877bfe4ed42c615e856
rncbc <rncbc@rncbc.org>
2023-10-25 19:23:36 +0100

    - Prepped Clip/Tools/Tempo ramp... for multi time-scale nodes, eventually...

--- src/qtractorTracks.cpp
+++ src/qtractorTracks.cpp
@@ -907,10 +907,12 @@ bool qtractorTracks::executeClipTool ( int iTool, qtractorClip *pClip )
 	}
 
 	// That's it...
-	qtractorTimeScaleAddNodeCommand *pTimeScaleAddNodeCommand
-		= toolsForm.timeScaleAddNodeCommand();
-	if (pTimeScaleAddNodeCommand)
-		pClipToolCommand->addTimeScaleNodeCommand(pTimeScaleAddNodeCommand);
+	qtractorTimeScaleNodeCommand *pTimeScaleNodeCommand
+		= toolsForm.timeScaleNodeCommand();
+	while (pTimeScaleNodeCommand) {
+		pClipToolCommand->addTimeScaleNodeCommand(pTimeScaleNodeCommand);
+		pTimeScaleNodeCommand = toolsForm.timeScaleNodeCommand();
+	}
 
 	return pSession->execute(pClipToolCommand);
 }
@@ -978,7 +980,7 @@ bool qtractorTracks::addClipToolCommand (
 
 	// Add new edit command from tool...
 	pClipToolCommand->addMidiEditCommand(
-		pMidiToolsForm->editCommand(pMidiClip, &select,
+		pMidiToolsForm->midiEditCommand(pMidiClip, &select,
 			pSession->tickFromFrame(pClip->clipStart()),
 			iTimeStart, iTimeEnd));
 			
